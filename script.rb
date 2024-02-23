8719bc155ae9d29479daabf6f1692fa951833da9
SimenM1 <156329770+SimenM1@users.noreply.github.com>
2024-02-23 16:45:36 +0000

    line 2 l changed to r fixed issues

--- script.rb
+++ script.rb
@@ -1,10 +1,10 @@
 
 def merge(left_sorted_array, right_sorted_array, l = 0, r = 0, result = [])
-  if left_sorted_array[l] == nil || right_sorted_array[l] == nil
+  if left_sorted_array[l] == nil && right_sorted_array[r] == nil
     return result
-  elsif left_sorted_array[l] == nil
+  elsif left_sorted_array == nil || left_sorted_array[l] == nil
     return result += right_sorted_array[r..-1]
-  elsif right_sorted_array[r] == nil
+  elsif right_sorted_array == nil || right_sorted_array[r] == nil
     return result += left_sorted_array[l..-1]
   elsif left_sorted_array[l] < right_sorted_array[r]
     result.push(left_sorted_array[l])
@@ -15,8 +15,8 @@ def merge(left_sorted_array, right_sorted_array, l = 0, r = 0, result = [])
   end
 end
 
-left_sorted_array = [1,6,8]
-right_sorted_array = [2,3,7]
+left_sorted_array = [1]
+right_sorted_array = [13]
 merge(left_sorted_array, right_sorted_array)
 
 def merge_sort(array)
@@ -24,10 +24,12 @@ def merge_sort(array)
     return array
   else
     left = array[0...(array.length/2)]
-    right = array[(array.length/2..-1)]
-    return merge(left, right)
+    right = array[(array.length/2)..-1]
+    p left_sorted_array = merge_sort(left)
+    p right_sorted_array = merge_sort(right)
+    return merge(left_sorted_array, right_sorted_array)
   end
 end
 
-array = [1,6,8,2,3,7]
+array = [3, 2, 1, 13, 8, 5, 0, 1]
 merge_sort(array)
